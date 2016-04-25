9265f92f5ea5bc8ee0500fa98a59ed6bf8e9b580
Jeffrey Moran <Jmc113093@gmail.com>
2016-04-24 23:18:19 -0400

    Fixed "Mutating a managed object ... after it has been removed from its context." bug

--- Homepwner/BrokersLabItemStore.m
+++ Homepwner/BrokersLabItemStore.m
@@ -13,132 +13,135 @@
 
 + (BrokersLabItemStore *)sharedStore
 {
-    static BrokersLabItemStore *sharedStore = nil;
-    if(!sharedStore)
-        sharedStore = [[super allocWithZone:nil] init];
-        
-    return sharedStore;
+	static BrokersLabItemStore *sharedStore = nil;
+	if(!sharedStore)
+		sharedStore = [[super allocWithZone:nil] init];
+	
+	return sharedStore;
 }
 
 + (id)allocWithZone:(NSZone *)zone
 {
-    return [self sharedStore];
+	return [self sharedStore];
 }
 
-- (id)init 
+- (id)init
 {
-    self = [super init];
-    if(self) {                
-        model = [NSManagedObjectModel mergedModelFromBundles:nil];
+	self = [super init];
+	if(self) {
+		model = [NSManagedObjectModel mergedModelFromBundles:nil];
 		
-        NSPersistentStoreCoordinator *psc = 
-        [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
-        
-        NSString *path = [self itemArchivePath];
-        NSURL *storeURL = [NSURL fileURLWithPath:path]; 
-        
-        NSError *error = nil;
-        
-        if (![psc addPersistentStoreWithType:NSSQLiteStoreType 
-                               configuration:nil
-                                         URL:storeURL
-                                     options:nil
-                                       error:&error]) {
-            [NSException raise:@"Open failed"
-                        format:@"Reason: %@", [error localizedDescription]];
-        }
-        
-        // Create the managed object context
+		NSPersistentStoreCoordinator *psc =
+		[[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:model];
+		
+		NSString *path = [self itemArchivePath];
+		NSURL *storeURL = [NSURL fileURLWithPath:path];
+		
+		NSError *error = nil;
+		
+		if (![psc addPersistentStoreWithType:NSSQLiteStoreType
+							   configuration:nil
+										 URL:storeURL
+									 options:nil
+									   error:&error]) {
+			[NSException raise:@"Open failed"
+						format:@"Reason: %@", [error localizedDescription]];
+		}
+		
+		// Create the managed object context
 		context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
-        [context setPersistentStoreCoordinator:psc];        
-        
-        // The managed object context can manage undo, but we don't need it
-        [context setUndoManager:nil];
-        
-        [self loadAllItems];        
-    }
-    return self;
+		[context setPersistentStoreCoordinator:psc];
+		
+		// The managed object context can manage undo, but we don't need it
+		[context setUndoManager:nil];
+		
+		[self loadAllItems];
+	}
+	return self;
 }
 
-- (void)loadAllItems 
+- (void)loadAllItems
 {
-    if (!allItems) {
-        NSFetchRequest *request = [[NSFetchRequest alloc] init];
-        
-        NSEntityDescription *entity = [[model entitiesByName] objectForKey:@"BrokersLabItem"];
-        [request setEntity:entity];
-        
-        NSSortDescriptor *sortDescriptor = [NSSortDescriptor 
-                                sortDescriptorWithKey:@"orderingValue"
-                                ascending:YES];
-        [request setSortDescriptors:[NSArray arrayWithObject:sortDescriptor]];
-        
-        NSError *error;
-        NSArray *result = [context executeFetchRequest:request error:&error];
-        if (!result) {
-            [NSException raise:@"Fetch failed"
-                        format:@"Reason: %@", [error localizedDescription]];
-        }
-        
-        allItems = [[NSMutableArray alloc] initWithArray:result];
-    }
+	if (!allItems) {
+		NSFetchRequest *request = [[NSFetchRequest alloc] init];
+		
+		NSEntityDescription *entity = [[model entitiesByName] objectForKey:@"BrokersLabItem"];
+		[request setEntity:entity];
+		
+		NSSortDescriptor *sortDescriptor = [NSSortDescriptor
+											sortDescriptorWithKey:@"orderingValue"
+											ascending:YES];
+		[request setSortDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+		
+		NSError *error;
+		NSArray *result = [context executeFetchRequest:request error:&error];
+		if (!result) {
+			[NSException raise:@"Fetch failed"
+						format:@"Reason: %@", [error localizedDescription]];
+		}
+		
+		allItems = [[NSMutableArray alloc] initWithArray:result];
+	}
 }
 
 - (NSString *)itemArchivePath
 {
-    NSArray *documentDirectories =
-        NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
-                                            NSUserDomainMask, YES);
+	NSArray *documentDirectories =
+	NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+										NSUserDomainMask, YES);
  
-       // Get one and only document directory from that list
-    NSString *documentDirectory = [documentDirectories objectAtIndex:0];
-
-    return [documentDirectory stringByAppendingPathComponent:@"store.data"];
+	// Get one and only document directory from that list
+	NSString *documentDirectory = [documentDirectories objectAtIndex:0];
+	
+	return [documentDirectory stringByAppendingPathComponent:@"store.data"];
 }
 
 - (BOOL)saveChanges
 {
-    NSError *err = nil;
-    BOOL successful = [context save:&err];
-    if (!successful) {
-        NSLog(@"Error saving: %@", [err localizedDescription]);
-    }
-    return successful;
+	NSError *err = nil;
+	BOOL successful = [context save:&err];
+	
+	[context performBlockAndWait:^{
+		if (!successful) {
+			NSLog(@"Error saving: %@", [err localizedDescription]);
+		}
+	}];
+	
+	return successful;
 }
 
-- (void)removeItem:(BrokersLabItem *)p
-{
-        [context deleteObject:p];
-    [allItems removeObjectIdenticalTo:p];
+- (void)removeItem:(BrokersLabItem *)p {
+	[context deleteObject:p];
+	[allItems removeObjectIdenticalTo:p];
 }
 
 - (NSArray *)allItems
 {
-    return allItems;
+	return allItems;
 }
 
 - (void)moveItemAtIndex:(NSInteger)from toIndex:(NSInteger)to
 {
-    if (from == to) {
-        return;
-    }
-    // Get pointer to object being moved so we can re-insert it
-    BrokersLabItem *p = [allItems objectAtIndex:from];
-
-    // Remove p from array
-    [allItems removeObjectAtIndex:from];
-
-    // Insert p in array at new location
-    [allItems insertObject:p atIndex:to];
+	if (from == to) {
+		return;
+	}
+	// Get pointer to object being moved so we can re-insert it
+	BrokersLabItem *p = [allItems objectAtIndex:from];
+	
+	// Remove p from array
+	[allItems removeObjectAtIndex:from];
+	
+	// Insert p in array at new location
+	[allItems insertObject:p atIndex:to];
 }
 
 - (BrokersLabItem *)createItem {
-    BrokersLabItem *newItem = [NSEntityDescription insertNewObjectForEntityForName:@"BrokersLabItem"
-                                                inManagedObjectContext:context];
+	BrokersLabItem *newItem = [NSEntityDescription insertNewObjectForEntityForName:@"BrokersLabItem"
+															inManagedObjectContext:context];
 	
 	[allItems addObject:newItem];
-   
-    return newItem;
+	
+	return newItem;
 }
 
 @end
