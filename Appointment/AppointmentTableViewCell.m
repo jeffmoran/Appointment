25e8b81b4924d163ce47f75691810261429a0cf1
Jeff Moran <Jmc113093@gmail.com>
2017-06-11 23:37:52 -0400

    If appointment time has passed, cell background color is a light red

--- Appointment/AppointmentTableViewCell.m
+++ Appointment/AppointmentTableViewCell.m
@@ -89,6 +89,10 @@
 	} else {
 		appointmentTimeLabel.text = @"Appointment time unavailable";
 	}
+
+	if ([self.appointment.appointmentTime timeIntervalSinceNow] < 0.0) {
+		self.backgroundColor = [UIColor colorWithRed:255.0 / 255.0 green:0.0 blue:0.0 alpha:0.1];
+	}
 }
 
 @end
